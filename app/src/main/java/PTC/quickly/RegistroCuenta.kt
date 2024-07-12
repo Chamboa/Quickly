92ff10ec243523ebedc35cc79a9a018527fba1a1
Mateo <20230131@ricaldone.edu.sv>
2024-07-12 12:03:36 -0600

    Avance de pantalla

--- app/src/main/java/PTC/quickly/RegistroCuenta.kt
+++ app/src/main/java/PTC/quickly/RegistroCuenta.kt
@@ -1,11 +1,16 @@
 package PTC.quickly
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import kotlinx.coroutines.Dispatchers
@@ -16,6 +21,15 @@ import modelo.ClaseConexion
 import java.util.UUID
 
 class RegistroCuenta : AppCompatActivity() {
+
+    val codigo_opcional_galeria = 102
+    val STORAGE_REQUEST_CODE = 1
+
+    lateinit var imageView: ImageView
+    lateinit var miPath: String
+
+    val uuid = UUID.randomUUID().toString()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -26,11 +40,60 @@ class RegistroCuenta : AppCompatActivity() {
             insets
         }
         //1- mandar a llamar a todos los elementos de la vista
+        imageView = findViewById(R.id.imageView)
         val txtNombreRegistro = findViewById<TextView>(R.id.txtNombreRegistro)
         val txtCorreoRegistro = findViewById<TextView>(R.id.txtCorreoRegistro)
         val txtContraseñaRegistro = findViewById<TextView>(R.id.txtContraseñaRegistro)
         val txtConfirmarContraseñaRegistro = findViewById<TextView>(R.id.txtConfirmarContraseñaRegistro)
         val btnCrearCuenta = findViewById<Button>(R.id.btnCrearCuenta)
+        val btnSubirFoto = findViewById<Button>(R.id.btnSubirFoto)
+
+        btnSubirFoto.setOnClickListener{
+            checkStoragePermission()
+        }
+
+        private fun checkStorePermission() {
+            if (ContextCompat.CheckSelPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != packageManager.PERMISSION_GRANTED) {
+                pedirPermisoAlmacenamiento()
+            } else {
+                val intent = intent(Intent.ACTION_PICK)
+                intent.type = "image/*"
+                startActivityForResult(intent, codigo_opcional_galeria)
+            }
+        }
+
+        private fun pedirPermisoAlmacenamiento() {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),STORAGE_REQUEST_CODE)
+            }
+        }
+
+        override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<String>, grantResults: IntArray
+        ) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            when (requestCode) {
+                STORAGE_REQUEST_CODE -> {
+                    if ((grantResultsisNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                        val intent = Intent(Intent.ACTION_PICK)
+                        intent.type = "image/*"
+                        startActivityForResult(intent, codigo_opcional_galeria)
+                    } else {
+                        Toast.makeText(this, "Permiso de almacenamiento denegado", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+                else -> {
+
+                }
+            }
+        }
+
+
+
 
         //2- programar el boton de crear
         //TODO: Boton para crear la cuenta//
