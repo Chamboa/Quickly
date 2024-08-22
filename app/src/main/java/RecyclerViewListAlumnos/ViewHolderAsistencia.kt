415649fee6774884c6e33d722c8c2a80f5f2f396
SamSS <20230372@ricaldone.edu.sv>
2024-08-22 15:44:05 -0600

    se arreglaron unos detalles

--- app/src/main/java/RecyclerViewListAlumnos/ViewHolderAsistencia.kt
+++ app/src/main/java/RecyclerViewListAlumnos/ViewHolderAsistencia.kt
@@ -1,11 +1,11 @@
 package PTC.quickly
 
-import PTC.quickly.R
 import android.view.View
 import android.widget.CheckBox
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+
 
 
 class ViewHolderAsistencia(view: View) : RecyclerView.ViewHolder(view) {
@@ -16,7 +16,7 @@ class ViewHolderAsistencia(view: View) : RecyclerView.ViewHolder(view) {
 
     val imgAgregar_Horas: ImageView = view.findViewById(R.id.imgAgregarHoras)
 
-    val checkBox: CheckBox = view.findViewById(R.id.checkBox)
+    val checkBox: CheckBox = view.findViewById(R.id.check_si)
 
 
 
